3f68ea38bd546d583f92c95bbf0afec25ea9d0d2
Mozammel Haque <mozammel@livingoncodes.com>
2014-11-22 19:34:49 +0600

    Adding Apache Commons DBCP

---
+++ springtutorial39/src/main/java/com/livingoncodes/spring/springtutorial/Parrot.java
@@ -0,0 +1,28 @@
+package com.livingoncodes.spring.springtutorial;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class Parrot {
+
+	private String id = "123";
+	private String speech = "hi there";
+
+	@Autowired
+	public void setId(@Value("${jdbc.user}") String id) {
+		this.id = id;
+	}
+
+	@Autowired
+	public void setSpeech(@Value("${jdbc.password}") String speech) {
+		this.speech = speech;
+	}
+	
+	public void speak() {
+		System.out.println(id + ":" + speech);
+	}
+
+}
