7f6db5ae27c1dc95f90fdc16f0938104623f7096
Mozammel Haque <mozammel@livingoncodes.com>
2014-11-23 09:07:16 +0600

    Using Named Parameters

---
+++ springtutorial42/src/main/java/com/livingoncodes/spring/springtutorial/NoticesDAO.java
@@ -0,0 +1,66 @@
+package com.livingoncodes.spring.springtutorial;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.stereotype.Component;
+
+@Component("noticesDao")
+public class NoticesDAO {
+
+	private NamedParameterJdbcTemplate jdbc;
+
+	@Autowired
+	public void setDataSource(DataSource jdbc) {
+		this.jdbc = new NamedParameterJdbcTemplate(jdbc);
+	}
+
+	public List<Notice> getNotices() {
+		
+		return jdbc.query("select * from notices", new RowMapper<Notice>() {
+
+			public Notice mapRow(ResultSet rs, int rowNum) throws SQLException {
+				Notice notice = new Notice();
+				
+				notice.setId(rs.getInt("id"));
+				notice.setName(rs.getString("name"));
+				notice.setEmail(rs.getString("email"));
+				notice.setText(rs.getString("text"));
+				
+				return notice;
+			}
+
+		});
+
+	}
+	
+	public Notice getNotice(int id) {
+		
+		MapSqlParameterSource params = new MapSqlParameterSource();
+		params.addValue("id", id);
+
+		return jdbc.queryForObject("select * from notices where id = :id", params, new RowMapper<Notice>() {
+
+			public Notice mapRow(ResultSet rs, int rowNum) throws SQLException {
+				Notice notice = new Notice();
+				
+				notice.setId(rs.getInt("id"));
+				notice.setName(rs.getString("name"));
+				notice.setEmail(rs.getString("email"));
+				notice.setText(rs.getString("text"));
+				
+				return notice;
+			}
+
+		});
+
+	}
+}
