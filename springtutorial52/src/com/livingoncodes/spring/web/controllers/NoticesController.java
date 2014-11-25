80411c94c54c5691f584da74340fab9ed2210f69
Mozammel Haque <mozammel@livingoncodes.com>
2014-11-25 10:47:02 +0600

    adding View Resolver

---
+++ springtutorial52/src/com/livingoncodes/spring/web/controllers/NoticesController.java
@@ -0,0 +1,13 @@
+package com.livingoncodes.spring.web.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class NoticesController {
+
+	@RequestMapping("/")
+	public String  showHome() {
+		return "home";
+	}
+}
