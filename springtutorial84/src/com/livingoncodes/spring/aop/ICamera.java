80ce14352de738cae50744825291caef299c60c9
Mozammel Haque <mozammel@livingoncodes.com>
2014-12-05 20:12:19 +0600

    The Bean PCD

---
+++ springtutorial84/src/com/livingoncodes/spring/aop/ICamera.java
@@ -0,0 +1,16 @@
+package com.livingoncodes.spring.aop;
+
+public interface ICamera {
+
+	public abstract void snap();
+
+	public abstract void snap(int exposure);
+
+	public abstract String snap(String name);
+
+	public abstract void snapNighttime();
+
+	public abstract void snapCar(Car car);
+
+
+}
---
+++ springtutorial84/src/com/livingoncodes/spring/aop/Logger.java
@@ -0,0 +1,24 @@
+package com.livingoncodes.spring.aop;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class Logger {
+	
+	
+	 @Pointcut("bean(*e*)")
+	 public void somePointcut() {
+	 }
+	
+	
+	 @Before("somePointcut()")
+	 public void somePointcutDemo() {
+	 System.out.println("********** BEFORE DEMO ************");
+	 }
+
+	
+}
