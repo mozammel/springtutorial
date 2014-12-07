91f0e0d08526f2d5692368975e3671497aaed623
Mozammel Haque <mozammel@livingoncodes.com>
2014-12-07 23:04:16 +0600

    Adding a spring security filter

---
+++ springtutorial91/src/com/livingoncodes/spring/web/service/NoticesService.java
@@ -0,0 +1,36 @@
+package com.livingoncodes.spring.web.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.livingoncodes.spring.web.dao.Notice;
+import com.livingoncodes.spring.web.dao.NoticesDAO;
+
+@Service("noticesService")
+public class NoticesService {
+	
+	private NoticesDAO noticesDAO;
+
+	@Autowired
+	public void setNoticesDAO(NoticesDAO noticesDAO) {
+		this.noticesDAO = noticesDAO;
+	}
+	
+	public List<Notice> getCurrent() {
+		return noticesDAO.getNotices();
+	}
+
+	public void create(Notice notice) {
+		noticesDAO.create(notice);
+		
+	}
+
+	public void throwTestException() {
+		noticesDAO.getNotice(98383);
+	}
+	
+	
+
+}
