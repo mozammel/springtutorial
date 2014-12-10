13b2ad33f185b34c0c35f6a021c35ad5f0a7bfeb
Mozammel Haque <mozammel@livingoncodes.com>
2014-12-10 18:04:45 +0600

    Outputting text based on authentication status

---
+++ springtutorial105/src/com/livingoncodes/spring/web/controllers/HomeController.java
@@ -0,0 +1,21 @@
+package com.livingoncodes.spring.web.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+	
+	@RequestMapping("/")
+	public String  showHome() {
+		return "home";
+	}
+	
+	@RequestMapping("/admin")
+	public String  shoAdmin() {
+		return "admin";
+	}
+	
+	
+}
