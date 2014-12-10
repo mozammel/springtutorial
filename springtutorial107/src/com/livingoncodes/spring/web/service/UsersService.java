09c8f36d88fe7e65cfd0eb0f3092e1b1e05db3e3
Mozammel Haque <mozammel@livingoncodes.com>
2014-12-10 19:14:38 +0600

    Row mapping with Bean Property Row Mapper

---
+++ springtutorial107/src/com/livingoncodes/spring/web/service/UsersService.java
@@ -0,0 +1,43 @@
+package com.livingoncodes.spring.web.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.livingoncodes.spring.web.dao.User;
+import com.livingoncodes.spring.web.dao.UsersDao;
+
+@Service("usersService")
+public class UsersService {
+	
+	private UsersDao usersDao;
+
+	
+	@Autowired
+	public void setUsersDao(UsersDao usersDao) {
+		this.usersDao = usersDao;
+	}
+
+
+
+	public void create(User user) {
+		usersDao.create(user);
+		
+	}
+
+
+
+	public boolean exists(String username) {
+		return usersDao.exists(username);
+	}
+
+
+
+	public List<User> getAllUsers() {
+		return usersDao.getAllUsers();
+	}
+
+
+
+}
