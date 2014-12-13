a8f78b54c49ad75f8abd64420649b5e695049838
Mozammel Haque <mozammel@livingoncodes.com>
2014-12-13 10:27:16 +0600

    Creating tiles from jsp pages

---
+++ springtutorial117/WebContent/WEB-INF/tiles/header.jsp
@@ -0,0 +1,22 @@
+ <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+ 
+    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">Notices</a>
+        </div>
+        <div id="navbar" class="collapse navbar-collapse">
+          <ul class="nav navbar-nav">
+          	
+            <li class="active"><a href="<c:url value='/' />">Home</a></li>
+    
+          </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+    </nav>
---
+++ springtutorial117/src/com/livingoncodes/spring/web/validation/ValidEmail.java
@@ -0,0 +1,34 @@
+package com.livingoncodes.spring.web.validation;
+
+import static java.lang.annotation.ElementType.ANNOTATION_TYPE;
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+
+
+@Target({ METHOD, FIELD, ANNOTATION_TYPE })
+@Retention(RUNTIME)
+@Documented
+@Constraint(validatedBy = {com.livingoncodes.spring.web.validation.ValidEmailImpl.class})
+public @interface ValidEmail {
+
+	String message() default "That does not seem to be a valid email address.";
+
+	Class<?>[] groups() default { };
+
+	Class<? extends Payload>[] payload() default { };
+
+	/**
+	 * @return size the element must be higher or equal to
+	 */
+	int min() default 5;
+
+
+}
