882855615a07368d083005543fd4f9c941df66c5
Mozammel Haque <mozammel@livingoncodes.com>
2014-12-13 20:48:05 +0600

    Coding the JUnit Dao Tests

---
+++ springtutorial125/test/com/livingoncodes/spring/web/test/tests/UserDaoTests.java
@@ -0,0 +1,53 @@
+package com.livingoncodes.spring.web.test.tests;
+
+import static org.junit.Assert.*;
+
+import javax.sql.DataSource;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.livingoncodes.spring.web.dao.User;
+import com.livingoncodes.spring.web.dao.UsersDao;
+
+@ActiveProfiles("dev")
+@ContextConfiguration(locations = {
+		"classpath:com/livingoncodes/spring/web/config/dao-context.xml",
+		"classpath:com/livingoncodes/spring/web/config/security-context.xml",
+		"classpath:com/livingoncodes/spring/web/test/config/datasource.xml"})
+@RunWith(SpringJUnit4ClassRunner.class)
+public class UserDaoTests {
+	
+	@Autowired
+	private UsersDao usersDao;
+	
+	@Autowired
+	private DataSource dataSource;
+	
+	@Before
+	public void init() {
+		JdbcTemplate jdbc = new JdbcTemplate(dataSource);
+		
+		jdbc.execute("delete from users");
+		jdbc.execute("delete from authorities");
+		
+	}
+	
+	
+	@Test
+	public void testCreateUser() {
+		
+		User user = new User("mozammel", "hellohello", "mozammel@livingoncodes.com", true, "ROLE_USER");
+		
+		
+		
+		assertTrue("User creation should return true", usersDao.create(user));
+	}
+
+}
