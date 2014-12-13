6b54c39d78b36f490eaa50a431dfc2d9d9f7f34b
Mozammel Haque <mozammel@livingoncodes.com>
2014-12-13 20:47:41 +0600

    Creating JUnit tests

---
+++ springtutorial124/src/com/livingoncodes/spring/web/controllers/HomeController.java
@@ -0,0 +1,23 @@
+package com.livingoncodes.spring.web.controllers;
+
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+	private static Logger logger = Logger.getLogger(HomeController.class);
+	
+	@RequestMapping("/")
+	public String  showHome() {
+		
+		logger.info("Showing home...");
+		
+		return "home";
+	}
+	
+
+	
+	
+}
