dd3a7613271104902d693cfe9caac5bb2ea1837d
Mozammel Haque <mozammel@livingoncodes.com>
2014-12-15 20:08:19 +0600

    Multiple form submits and optional parameters

---
+++ springtutorial134/WebContent/WEB-INF/tiles/noticecreated.jsp
@@ -0,0 +1,6 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+
+
+Notice created. <a href="${pageContext.request.contextPath}/notices">Current Notices</a>
---
+++ springtutorial134/src/com/livingoncodes/spring/web/controllers/ErrorHandler.java
@@ -0,0 +1,23 @@
+package com.livingoncodes.spring.web.controllers;
+
+import org.springframework.dao.DataAccessException;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class ErrorHandler {
+	@ExceptionHandler(DataAccessException.class)
+	public String handleDatabaseException(DataAccessException ex) {
+		ex.printStackTrace();
+		return "error";
+	}
+	
+	
+	@ExceptionHandler(AccessDeniedException.class)
+	public String handleAccessDeniedException(AccessDeniedException ex) {
+		return "denied";
+	}
+	
+
+}
