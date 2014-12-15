6c48940b5cb21ec95bf0d7f8286f9a05b5b3fa9f
Mozammel Haque <mozammel@livingoncodes.com>
2014-12-15 18:20:53 +0600

    Conditional texts in JSPs

---
+++ springtutorial132/src/com/livingoncodes/spring/web/service/NoticesService.java
@@ -0,0 +1,47 @@
+package com.livingoncodes.spring.web.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.stereotype.Service;
+
+import com.livingoncodes.spring.web.dao.Notice;
+import com.livingoncodes.spring.web.dao.NoticesDao;
+
+@Service("noticesService")
+public class NoticesService {
+	
+	private NoticesDao noticesDAO;
+
+	@Autowired
+	public void setNoticesDAO(NoticesDao noticesDAO) {
+		this.noticesDAO = noticesDAO;
+	}
+	
+	public List<Notice> getCurrent() {
+		return noticesDAO.getNotices();
+	}
+
+	@Secured({"ROLE_USER", "ROLE_ADMIN"})
+	public void create(Notice notice) {
+		noticesDAO.create(notice);
+		
+	}
+
+	public boolean hasNotice(String name) {
+		if( name == null )
+			return false;
+		
+		List<Notice> notices = noticesDAO.getNotices(name);
+		
+		if(notices.size() == 0)
+			return false;
+		
+		
+		return true;
+	}
+
+
+
+}
