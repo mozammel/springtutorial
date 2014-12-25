39400a857971617e9ec0bb6ee3e806b777082f80
Mozammel Haque <mozammel@livingoncodes.com>
2014-12-25 21:14:08 +0600

    Transitions

---
+++ springtutorial154/src/com/livingoncodes/spring/web/dao/NoticesDao.java
@@ -0,0 +1,92 @@
+package com.livingoncodes.spring.web.dao;
+
+import java.util.List;
+
+import javax.sql.DataSource;
+
+import org.hibernate.Criteria;
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.criterion.Restrictions;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+@Repository
+@Transactional
+@Component("noticesDao")
+public class NoticesDao {
+
+	private NamedParameterJdbcTemplate jdbc;
+
+	@Autowired
+	private SessionFactory sessionFactory;
+	
+	
+	public Session session() {
+		return sessionFactory.getCurrentSession();
+	}
+	
+
+	@SuppressWarnings("unchecked")
+	public List<Notice> getNotices() {
+		
+		Criteria crit = session().createCriteria(Notice.class);
+		
+		crit.createAlias("user", "u").add(Restrictions.eq("u.enabled", true));
+		
+		return crit.list();
+
+	}
+	
+	@SuppressWarnings("unchecked")
+	public List<Notice> getNotices(String username) {
+		
+		Criteria crit = session().createCriteria(Notice.class);
+		
+		crit.createAlias("user", "u");
+		
+		crit.add(Restrictions.eq("u.enabled", true));
+		crit.add(Restrictions.eq("u.username", username));
+		
+		
+		return crit.list();
+		
+	}
+
+	public boolean delete(int id) {
+
+		Query query = session().createQuery("delete from Notice where id=:id");
+		query.setLong("id", id);
+		return query.executeUpdate() == 1;
+		
+	}
+	
+
+
+	public void saveOrUpdate(Notice notice) {
+
+		session().saveOrUpdate(notice);
+	}
+	
+	
+	public Notice getNotice(int id) {
+
+		Criteria crit = session().createCriteria(Notice.class);
+		
+		crit.createAlias("user", "u");
+		
+		crit.add(Restrictions.eq("u.enabled", true));
+		crit.add(Restrictions.idEq(id));
+		
+		
+		return (Notice) crit.uniqueResult();
+		
+
+	}
+
+}
